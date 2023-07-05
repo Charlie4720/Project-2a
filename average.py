ebee5157f60ffa726f540f08b312e3921b5bd640
Charlie4720 <137825941+Charlie4720@users.noreply.github.com>
2023-07-05 15:26:26 -0700

    Update average.py

--- average.py
+++ average.py
@@ -1,11 +1,12 @@
-# Prompt the user to enter 5 numbers
+# Prompt the user to enter five numbers
 numbers = []
 for i in range(5):
-    num = float(input("Enter number {}: ".format(i+1)))
+    num = float(input(f"Enter number {i+1}: "))
     numbers.append(num)
 
-# Calculate the average
+# Calculate the average of the numbers
 average = sum(numbers) / len(numbers)
 
-# Print the average to 1 decimal place
-print("The average is:", round(average, 1))
+# Display the average to 7 decimal places
+formatted_average = "{:.7f}".format(average)
+print("Average:", formatted_average)
